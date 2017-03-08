067b85bc78d633391812bfb7d8f1fe1eac1b0b0e
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-03-08 12:48:07 -0800

    Added remove hobby and movie actions

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -17,33 +17,121 @@ var redux = require('redux');
 console.log('starting redux todo example');
 
 var stateDefault = {
-  searchText: '',
-  showCompleted: false,
-  todos: []
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
 };
 
+var nextHobbyId = 1,
+  nextMovieId = 1;
 var reducer = (state = stateDefault, action) => {
   switch(action.type) {
     case 'CHANGE_NAME':
       return {
         ...state,
         name: action.name
-      }
+      };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies.filter((hobby) => {
+            return hobby.id !== action.id
+          })
+        ]
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies.filter((movie) => {
+            return movie.id !== action.id
+          })
+        ]
+      };
+
     default:
       return state;
   }
   return state;
 };
-var store = redux.createStore(reducer);
+
+// 'redux.compose' allows for the redux chrome extension to work
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-var action = {
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+  console.log('name is', state.name);
+  document.getElementById('app').innerHTML = state.name;
+  console.log('new state is', state);
+})
+
+var currentState = store.getState();
+console.log('currentState', currentState);
+
+store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Joel'
-}
+});
 
-store.dispatch(action);
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
 
-console.log("name should be Joel", store.getState());
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Barf Lord',
+  genre: 'RomHor'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
+});
+
+// unsubscribe();
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Bob'
+});
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -18,16 +18,43 @@ var reducer = (state = stateDefault, action) => {
       return state;
   }
 };
-var store = redux.createStore(reducer);
+
+// 'redux.compose' allows for the redux chrome extension to work
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+store.subscribe(() => {
+  var state = store.getState();
+  console.log('name is', state.name);
+})
 
 var currentState = store.getState();
 console.log('currentState', currentState);
 
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+  console.log('searchText is', state.searchText);
+  document.getElementById('app').innerHTML = state.searchText;
+})
+
 var action = {
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'work'
+};
+
+store.dispatch(action);
+
+action = {
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'Joel'
 };
 
 store.dispatch(action);
 
-console.log("searchText should be Joel", store.getState());
+action = {
+  type: 'CHANGE_SEARCH_TEXT',
+  name: 'Joel'
+};
